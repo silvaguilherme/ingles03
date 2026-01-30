fcf111d49ee8b3736677a0a2e97588a7d7187622
Guilherme Silva! <silva.guilherme@outlook.com>
2026-01-30 16:26:35 -0300

    Add profile routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\ProgressController;
+use App\Http\Controllers\ProfileController;
 
 Route::redirect('/', '/courses');
 
@@ -17,6 +18,10 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/lessons/{lesson}', [LessonController::class, 'show'])->name('lessons.show');
     Route::post('/progress', [ProgressController::class, 'store'])->name('progress.store');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
